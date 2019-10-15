319ce39145288b05ad17ac161c18457da2c96532
kevin zhuang <43941510+kazhala@users.noreply.github.com>
2019-10-15 16:34:52 +1100

    ADD routes and boots trap

---
+++ src/Routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import SignUp from './user/SignUp';
+import SignIn from './user/SignIn';
+import Home from './core/Home';
+
+const Routes = props => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/signin" exact component={SignIn} />
+                <Route path="/signup" exact component={SignUp} />
+            </Switch>
+        </BrowserRouter>
+    );
+};
+
+export default Routes;
---
+++ src/user/SignIn.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignIn = props => {
+    return <div>SignIn</div>;
+};
+
+export default SignIn;
