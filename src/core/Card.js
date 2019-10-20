33bbc6e06e64938f89432a9ead3c47d5a5a9ede0
kevin zhuang <43941510+kazhala@users.noreply.github.com>
2019-10-20 16:09:27 +1100

    added single product page

--- src/core/Card.js
+++ src/core/Card.js
@@ -5,22 +5,36 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import ShowImage from './ShowImage';
 
-const Card = ({ product }) => {
+const Card = ({ product, showViewProductButton = true }) => {
+    const showViewButton = () => {
+        return (
+            showViewProductButton && (
+                <Link to={`/product/${product._id}`}>
+                    <button className="btn btn-outline-primary mt-2 mb-2 mr-2">
+                        View Product
+                    </button>
+                </Link>
+            )
+        );
+    };
+
     return (
         <div className="card">
             <div className="card-header">{product.name}</div>
             <div className="card-body">
                 <ShowImage item={product} url="product" />
-                <p>{product.description.substring(0, 100)}</p>
-                <p>${product.price}</p>
-                <Link to={`/product/${product._id}`}>
-                    <button className="btn btn-outline-primary mt-2 mb-2 mr-2">
-                        View Product
-                    </button>
-                    <button className="btn btn-outline-warning mt-2 mb-2">
-                        Add to cart
-                    </button>
-                </Link>
+                <p className="lead mt-2">
+                    {product.description.substring(0, 100)}
+                </p>
+                <p className="black-9">${product.price}</p>
+                <p className="black-8">
+                    Category: {product.category && product.category.name}
+                </p>
+                <p className="black-8">Added on {}</p>
+                {showViewButton()}
+                <button className="btn btn-outline-warning mt-2 mb-2">
+                    Add to cart
+                </button>
             </div>
         </div>
     );
--- src/core/Product.js
+++ src/core/Product.js
@@ -33,7 +33,9 @@ const Product = props => {
             className="container-fluid"
         >
             <div className="row">
-                {product && product.description && <Card product={product} />}
+                {product && product.description && (
+                    <Card product={product} showViewProductButton={false} />
+                )}
             </div>
         </Layout>
     );
