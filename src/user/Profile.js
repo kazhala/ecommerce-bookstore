0d4fd12615821c0d866f48317cf626eac6cfec8e
kevin zhuang <43941510+kazhala@users.noreply.github.com>
2019-10-25 08:09:07 +1100

    fetch userData and update user profile data

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -14,10 +14,34 @@ const initialState = {
 
 const reducer = (state, action) => {
     switch (action.type) {
+        case 'error':
+            return { ...state, error: action.value };
+        case 'userData':
+            return {
+                ...state,
+                name: action.payload.name,
+                email: action.payload.email
+            };
+        case 'name':
+            return { ...state, error: '', name: action.value };
+        case 'email':
+            return { ...state, error: '', email: action.value };
+        case 'password':
+            return { ...state, error: '', password: action.value };
+        case 'updateSuccess':
+            return {
+                ...state,
+                error: '',
+                success: true,
+                name: action.payload.name,
+                email: action.payload.email
+            };
         default:
             return state;
     }
 };
+
+const { token } = isAuthenticated();
 
 const Profile = props => {
     const [userState, dispatch] = useReducer(reducer, initialState);
@@ -25,11 +49,91 @@ const Profile = props => {
 
     useEffect(() => {
         const init = userId => {
-            console.log(userId);
+            read(userId, token).then(res => {
+                if (res.error) {
+                    dispatch({ type: 'error', value: res.error });
+                } else {
+                    dispatch({
+                        type: 'userData',
+                        payload: { name: res.name, email: res.email }
+                    });
+                }
+            });
         };
 
         init(props.match.params.userId);
     }, []);
+
+    const handleChange = (e, value) => {
+        dispatch({ type: value, value: e.target.value });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        update(props.match.params.userId, token, {
+            name,
+            email,
+            password
+        }).then(res => {
+            if (res.error) {
+                dispatch({ type: 'error', value: res.error });
+            } else {
+                updateUser(res, () => {
+                    dispatch({
+                        type: 'updateSuccess',
+                        payload: {
+                            name: res.name,
+                            email: res.email
+                        }
+                    });
+                });
+            }
+        });
+    };
+
+    useEffect(() => {
+        const redirectUser = success => {
+            if (success) {
+                props.history.push('/user/dashboard');
+            }
+        };
+        redirectUser(success);
+    }, [success]);
+
+    const profileUpdate = (name, email, password) => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input
+                    type="text"
+                    onChange={e => handleChange(e, 'name')}
+                    className="form-control"
+                    value={name}
+                />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input
+                    type="text"
+                    onChange={e => handleChange(e, 'email')}
+                    className="form-control"
+                    value={email}
+                />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input
+                    type="text"
+                    onChange={e => handleChange(e, 'password')}
+                    className="form-control"
+                    value={password}
+                />
+            </div>
+            <button className="btn btn-primary" onClick={handleSubmit}>
+                Submit
+            </button>
+        </form>
+    );
 
     return (
         <Layout
@@ -38,6 +142,7 @@ const Profile = props => {
             className="container-fluid"
         >
             <h2 className="mb-4">Profile update</h2>
+            {profileUpdate(name, email, password)}
         </Layout>
     );
 };
