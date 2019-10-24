f839f3597f1feb04ff22ef24068651dbedc58c7b
kevin zhuang <43941510+kazhala@users.noreply.github.com>
2019-10-24 12:54:28 +1100

    resolved re-render issue and add update status functions

--- src/admin/Orders.js
+++ src/admin/Orders.js
@@ -1,25 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
-import { listOrders } from './apiAdmin';
+import { listOrders, getStatusValues, updateOrderStatus } from './apiAdmin';
 import moment from 'moment';
+const { user, token } = isAuthenticated();
 
 const Orders = props => {
     const [orders, setOrders] = useState([]);
+    const [statusValues, setStatusValues] = useState([]);
+
+    const loadOrders = useCallback((userId, token) => {
+        listOrders(userId, token).then(res => {
+            if (res.error) {
+                console.log(res.error);
+            } else {
+                setOrders(res);
+            }
+        });
+    }, []);
 
     useEffect(() => {
-        const loadOrders = (userId, token) => {
-            listOrders(userId, token).then(res => {
-                if (res.error) {
-                    console.log(res.error);
-                } else {
-                    setOrders(res);
-                }
-            });
-        };
-        const { user, token } = isAuthenticated();
         loadOrders(user._id, token);
-    }, []);
+        getStatusValues(user._id, token).then(res => {
+            if (res.error) {
+                console.log(res.error);
+            } else {
+                setStatusValues(res);
+            }
+        });
+    }, [user, token]);
 
     const showOrdersLength = () => {
         if (orders.length > 0) {
@@ -47,11 +56,42 @@ const Orders = props => {
         </div>
     );
 
+    const showStatus = order => (
+        <div className="form-group">
+            <h3 className="mark mb-4">Status: {order.status}</h3>
+            <select
+                className="form-control"
+                onChange={e => handleStatusChange(e, order._id)}
+            >
+                <option value={order.status}>Update Status</option>
+                {statusValues.map((status, index) => (
+                    <option key={index} value={status}>
+                        {status}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+
+    const handleStatusChange = (e, orderId) => {
+        // console.log('Update order status');
+        updateOrderStatus(user._id, token, orderId, e.target.value).then(
+            res => {
+                if (res.error) {
+                    console.log('Status update failed');
+                } else {
+                    loadOrders(user._id, token);
+                }
+            }
+        );
+    };
+
     return (
         <Layout title="Orders" description="Manage orders here">
             <div className="row">
                 <div className="col-md-8 offset-md-2">
                     {showOrdersLength()}
+
                     {orders.map((order, orderIndex) => (
                         <div
                             className="mt-5"
@@ -65,7 +105,7 @@ const Orders = props => {
                             </h2>
                             <ul className="list-group mb-2">
                                 <li className="list-group-item">
-                                    {order.status}
+                                    {showStatus(order)}
                                 </li>
                                 <li className="list-group-item">
                                     Transaction ID: {order.transaction_id}
@@ -84,6 +124,7 @@ const Orders = props => {
                                     Delivery address: {order.address}
                                 </li>
                             </ul>
+
                             <h3 className="mt-4 mb-4 font-italic">
                                 Total products in the order:{' '}
                                 {order.products.length}
--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -58,3 +58,35 @@ export const listOrders = async (userId, token) => {
         console.log(err);
     }
 };
+
+export const getStatusValues = async (userId, token) => {
+    try {
+        const res = await fetch(`${API}/order/status-values/${userId}`, {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                Authorization: `Bearer ${token}`
+            }
+        });
+        return res.json();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export const updateOrderStatus = async (userId, token, orderId, status) => {
+    try {
+        const res = await fetch(`${API}/order/${orderId}/status/${userId}`, {
+            method: 'PUT',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify({ status, orderId })
+        });
+        return res.json();
+    } catch (err) {
+        console.log(err);
+    }
+};
